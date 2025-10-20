be6748b12e4b1f5c9ebf1a777900bd4d581fe02d
Miguel Angel Hernandez <miguel@nerdistan.com.ar>
2025-10-20 09:43:16 -0300

    revert: Attempt to fix deployment by reverting recent changes

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -3,12 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Set database configuration from DATABASE_URL environment variable.
-if ENV['DATABASE_URL']
-  Rails.application.config.database_configuration = {
-    'production' => {
-      'url' => ENV['DATABASE_URL']
-    }
-  }
-end
